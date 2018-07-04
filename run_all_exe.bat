94b8a783d1bf7faf44128976d7ff044c558ed2ff
Alexander Sysoyev <sysoyev@vmk.unn.ru>
2018-07-04 19:50:09 +0300

    Настройка интеграции с AppVeyor

--- run_all_exe.bat
+++ run_all_exe.bat
@@ -1,5 +1,5 @@
 rem del template/Debug/tests.exe
-del C:/projects/mp1-2018-2/template/Release/tests.exe
+del C:\\projects\\mp1-2018-2\\template\\Release\\tests.exe
 for /r %%a in (*.exe) do (
   echo "%%~fa"
   call "%%~fa"
